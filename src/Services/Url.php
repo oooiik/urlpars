b6d3554569cb9214fdbd5dbde7dfb1a46dd53d70
oooiik <toshevobidjon@gmail.com>
2021-11-20 13:20:54 +0500

    buildUrl

--- src/Services/Url.php
+++ src/Services/Url.php
@@ -55,36 +55,59 @@ class Url implements ContractUrl
         $this->query = $parse['query'] ?? null;
         $this->fragment = $parse['fragment'] ?? null;
 
-        if($this->host){
+        if ($this->host) {
             $domain = explode('.', $this->host);
             $this->domain_top = count($domain) ? array_pop($domain) : null;
             $this->domain_name = count($domain) ? array_pop($domain) : null;
             $this->domain_subs = $domain;
         }
 
-        if($this->path){
+        if ($this->path) {
             $path = pathinfo($this->path);
             $this->dirname = !in_array($path['dirname'], ['', null, '/', '\/', '\\']) ? $path['dirname'] : null;
             $this->extension = $path['extension'] ?? null;
             $this->file = $this->extension ? $path['basename'] : null;
             $this->filename = $this->file ? $path['filename'] : null;
-            if(!$this->file && $path['basename']) $this->dirname .= ($this->dirname ? '/' : '' ) . $path['basename'];
+            if (!$this->file && $path['basename']) $this->dirname .= ($this->dirname ? '/' : '') . $path['basename'];
         }
 
-        if($this->query){
+        if ($this->query) {
             parse_str($this->query, $this->attributes);
         }
+
+        return $this;
     }
 
     public function setUrl()
     {
-        $this->url .= $this->protocol ? $this->protocol.'://' : '';
+        $this->url .= $this->protocol ? $this->protocol . '://' : '';
         $this->url .= $this->host ?? '';
+        $this->url .= $this->path ?? '';
+        $this->url .= count($this->attributes) > 0 ? '?' . http_build_query($this->attributes) : '';
+        $this->url .= $this->fragment ? '#' . $this->fragment : '';
+
+        return $this;
+    }
+
+    public static function buildUrl(
+        string $protocol = null,
+        string $host,
+        string $path = null,
+        array $attributes = null,
+        string $fragment = null
+    ) {
+        $url = '';
+        $url .= $protocol ? $protocol . '://' : '';
+        $url .= $host ?? '';
+        $url .= $path ?? '';
+        $url .= count($attributes) > 0 ? '?' . http_build_query($attributes) : '';
+        $url .= $fragment ? '#' . $fragment : '';
+
+        return $url;
     }
 
     public function all()
     {
         return (object)get_object_vars($this);
     }
-
 }
